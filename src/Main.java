38a2120375e2dfefe4cdfc54d98aaea193800946
2m201 <melaniekelley@hotmail.com>
2020-08-31 21:07:38 +0200

    P2

---
+++ src/Main.java
@@ -0,0 +1,103 @@
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+       testReizigerDAO(new ReizigerDAOpsql(Main.getConnection()));
+
+    }
+//    public static void main(String[] args) {
+//        int number = 1;
+//
+//        try{
+//            //1. Get connection to database
+//            Connection myConn = DriverManager.getConnection("jdbc:postgresql:ovchip", "melanie", "Cookie20012");
+//            //2. Create a statement
+//            Statement myStmt = myConn.createStatement();
+//            //3. Execute SQL query
+//            ResultSet myRs = myStmt.executeQuery("select * from reiziger");
+//            //4. Process result set
+//
+//            System.out.println("Alle reizigers: ");
+//            while (myRs.next()) {
+//                if (myRs.getString("tussenvoegsel") == null) {
+//                    System.out.println("#"+ number + ": " + myRs.getString("voorletters") + ". " + myRs.getString("achternaam") + " (" + myRs.getString("geboortedatum") + ")");
+//
+//                } else {
+//                    System.out.println("#"+ number + ": " + myRs.getString("voorletters") + ". "  + myRs.getString("tussenvoegsel") + " "  + myRs.getString("achternaam") + " (" + myRs.getString("geboortedatum") + ")");
+//                }
+//                number +=1;
+//            }
+//
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    private static Connection getConnection() throws SQLException {
+        Connection myConn = DriverManager.getConnection("jdbc:postgresql:ovchip", "userA", "melanie");
+        return myConn;
+    }
+    private static void closeConnection() throws SQLException {
+        Connection myConn = DriverManager.getConnection("jdbc:postgresql:ovchip", "userA", "melanie");
+        myConn.close();
+    }
+
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        System.out.println("[Test] ReizigerDAO.findById(3) geeft de volgende reiziger: ");
+        System.out.println(rdao.findById(3));
+
+        System.out.println();
+
+//        Reiziger boris = new Reiziger(100, "B", "", "Gravens", java.sql.Date.valueOf("2002-09-17"));
+//        rdao.save(boris);
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        System.out.println();
+
+        System.out.println("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.delete(77) ");
+        rdao.delete(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        System.out.println();
+
+        System.out.println("[Test] ReizigerDAO.findByGbDatum(2002-09-17) geeft de volgende reizigers: ");
+        System.out.println(rdao.findByGbDatum("2002-09-17"));
+
+        System.out.println();
+
+        String naam = rdao.findById(100).getAchternaam();
+        System.out.println("[Test] Eerst had de persoon met ID 100 de achternaam " + naam + ", na ReizigerDAO.update()");
+        rdao.update(rdao.findById(100));
+        naam = rdao.findById(100).getAchternaam();
+        System.out.println(" heeft hij de achternaam " + naam);
+
+        Main.closeConnection();
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
+    }
+
+}
---
+++ src/ReizigerDAOpsql.java
@@ -0,0 +1,118 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+import static java.lang.Integer.parseInt;
+
+
+public class ReizigerDAOpsql implements ReizigerDAO  {
+    Connection myConn = DriverManager.getConnection("jdbc:postgresql:ovchip", "userA", "melanie");
+    Statement myStmt = myConn.createStatement();
+
+    public ReizigerDAOpsql(Connection conn) throws SQLException {
+    }
+
+    @Override
+    public boolean save(Reiziger reiziger) throws SQLException {
+        String q = "INSERT INTO reiziger (reiziger_id, voorletters, tussenvoegsel, achternaam, geboortedatum) values (?, ?, ?, ?, ?);";
+        PreparedStatement pst = myConn.prepareStatement(q);
+        try {
+            pst.setInt(1, reiziger.getId());
+            pst.setString(2, reiziger.getVoorletters());
+            pst.setString(3, reiziger.getTussenvoegsel());
+            pst.setString(4, reiziger.getAchternaam());
+            pst.setDate(5, (Date) reiziger.getGeboortedatum());
+            int rt = pst.executeUpdate();
+            return true;
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Reiziger reiziger)throws SQLException {
+        String q = "UPDATE reiziger SET achternaam=? WHERE reiziger_id =?;";
+        PreparedStatement pst = myConn.prepareStatement(q);
+
+        try{
+            pst.setString(1, "Gravens");
+            pst.setInt(2, reiziger.getId());
+            int mRst = pst.executeUpdate();
+            return true;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Reiziger reiziger) throws SQLException {
+        String q = "DELETE FROM reiziger WHERE reiziger_id= ?;"; // no need for * after delete, DELETE deletes everything already
+        PreparedStatement pst = myConn.prepareStatement(q);
+
+        try{
+            pst.setInt(1, reiziger.getId());
+            int myRs = pst.executeUpdate(); // use executeUpdate() instead of executeQuery() when you are not expecting information back (like SELECT)
+            return true;
+        }
+        catch(Exception e ) {e.printStackTrace();}
+
+
+        return false;
+    }
+
+    @Override
+    public Reiziger findById(int id) throws SQLException {
+        Reiziger r1 = null;
+        String q = "SELECT * FROM reiziger WHERE reiziger_id = ?;";
+        PreparedStatement pst = myConn.prepareStatement(q);
+        try{
+            pst.setInt(1, id);
+            ResultSet myRs= pst.executeQuery();
+
+            while (myRs.next()) {
+                r1 = new Reiziger(parseInt(myRs.getString("reiziger_id")), myRs.getString("voorletters"), myRs.getString("tussenvoegsel"), myRs.getString("achternaam"), Date.valueOf(myRs.getString("geboortedatum")));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return r1;
+    } // DONE
+
+    @Override
+    public List<Reiziger> findByGbDatum(String datum) throws SQLException {
+        ArrayList<Reiziger> alleReizigers = new ArrayList<>();
+        Reiziger r1 = null;
+
+        String q = "SELECT * FROM reiziger WHERE geboortedatum = ?;";
+        PreparedStatement pst = myConn.prepareStatement(q);
+        try{
+            pst.setDate(1, java.sql.Date.valueOf(datum));
+            ResultSet myRs= pst.executeQuery();
+
+            while (myRs.next()) {
+                r1 = new Reiziger(parseInt(myRs.getString("reiziger_id")), myRs.getString("voorletters"), myRs.getString("tussenvoegsel"), myRs.getString("achternaam"), Date.valueOf(myRs.getString("geboortedatum")));
+                alleReizigers.add(r1);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return alleReizigers;
+    }
+
+    @Override
+    public List<Reiziger> findAll() throws SQLException {
+        ArrayList<Reiziger> alleReizigers = new ArrayList<>();
+        ResultSet myRs = myStmt.executeQuery("select * from reiziger");
+
+        while (myRs.next()) {
+            Reiziger r1 = new Reiziger(parseInt(myRs.getString("reiziger_id")), myRs.getString("voorletters"), myRs.getString("tussenvoegsel"), myRs.getString("achternaam"), Date.valueOf(myRs.getString("geboortedatum")));
+            alleReizigers.add(r1);
+        }
+        return alleReizigers;
+    }
+    
+}
